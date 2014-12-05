ae579d8d03a5de90e19ec35196569531c2a0d004
TDo <info@kuestenschmiede.de>
2014-12-05 11:49:52 +0100

    Change versionnumber

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@
  * @license   GNU/LGPL http://opensource.org/licenses/lgpl-3.0.html
  * @copyright Küstenschmiede GmbH Software & Design 2014
  * @link      https://www.kuestenschmiede.de
- * @filesource 
+ * @filesource
  */
 
 
@@ -18,7 +18,7 @@
  * Global settings
  */
 $GLOBALS['con4gis_forum_extension']['installed']    = true;
-$GLOBALS['con4gis_forum_extension']['version']      = '3.0.0 beta';
+$GLOBALS['con4gis_forum_extension']['version']      = '3.0.0 rc1';
 
 /**
  * Frontend modules
@@ -28,7 +28,7 @@ array_insert( $GLOBALS['FE_MOD']['con4gis'], 5, array
 	'c4g_forum' 				=> 'Module_c4g_forum',
 	'c4g_forum_breadcrumb' 		=> 'Module_c4g_forum_breadcrumb',
 )
-);	
+);
 
 /**
  * Backend Modules
@@ -40,7 +40,7 @@ array_insert( $GLOBALS['BE_MOD']['con4gis'], 5, array
 		'tables' 		=> array('tl_c4g_forum'),
 		'build_index' 	=> array('C4GForumBackend', 'buildIndex'),
  		'icon'	 		=> 'system/modules/con4gis_forum/html/forumicon.png'
-	) 
+	)
 ));
 
 /**
