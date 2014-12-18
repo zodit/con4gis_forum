482c669e2811b2b6889743c30ee076393a3449c8
coastforge-tdo <tobias.dobbrunz@kuestenschmiede.de>
2014-12-18 14:28:10 +0100

    Set right versionnumber

--- config/config.php
+++ config/config.php
@@ -18,7 +18,7 @@
  * Global settings
  */
 $GLOBALS['con4gis_forum_extension']['installed']    = true;
-$GLOBALS['con4gis_forum_extension']['version']      = '3.0.0';
+$GLOBALS['con4gis_forum_extension']['version']      = '1.0.1';
 
 /**
  * Frontend modules
