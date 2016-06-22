8666792d88d29d1dfd0ffb5a2c58a2d3bb7e6368
coastforge-mei <info@kuestenschmiede.de>
2016-06-22 10:10:32 +0200

    next develop version

--- config/config.php
+++ config/config.php
@@ -15,7 +15,7 @@
  * Global settings
  */
 $GLOBALS['con4gis_forum_extension']['installed']    = true;
-$GLOBALS['con4gis_forum_extension']['version']      = '1.4.1-snapshot';
+$GLOBALS['con4gis_forum_extension']['version']      = '1.4.2-snapshot';
 
 /**
  * Frontend modules
